d43cbdf8ef47828d641a1888c4af46a6adff9938
Jerome Valette <jerome.valette@mypangee.com>
2023-04-29 16:21:30 +0200

    mise a jour : update by wifi

--- src/main.cpp
+++ src/main.cpp
@@ -135,9 +135,7 @@ String months[12] = {"Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juil
 
 void initWifi()
 {
-  // Nom de l'objet OTA
-  ArduinoOTA.setHostname("Litiere");
-  ArduinoOTA.setPassword("test");
+  
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
   {
     Serial.println("STA Failed to configure");
@@ -152,6 +150,41 @@ void initWifi()
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Nom de l'objet OTA
+  ArduinoOTA.setHostname("Litiere");
+  ArduinoOTA.setPassword("test");
+  ArduinoOTA.setPort(3232);
+  ArduinoOTA
+      .onStart([]()
+               {
+			String type;
+			if (ArduinoOTA.getCommand() == U_FLASH)
+				type = "sketch";
+			else // U_SPIFFS
+				type = "filesystem";
+
+			// NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+			Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+			Serial.printf("Error[%u]: ", error);
+			if (error == OTA_AUTH_ERROR)
+				Serial.println("Auth Failed");
+			else if (error == OTA_BEGIN_ERROR)
+				Serial.println("Begin Failed");
+			else if (error == OTA_CONNECT_ERROR)
+				Serial.println("Connect Failed");
+			else if (error == OTA_RECEIVE_ERROR)
+				Serial.println("Receive Failed");
+			else if (error == OTA_END_ERROR)
+				Serial.println("End Failed"); });
+
+  ArduinoOTA.setTimeout(60000);
 }
 
 void analogicSetup()
